03a6168e93b87f91632045b8c090a68994881cd1
faclonshivam <faclonshivam1213@gmail.com>
2025-12-27 15:57:00 +0530

    table creation

--- 7 Qestion testing/table creation.py
+++ 7 Qestion testing/table creation.py
@@ -1,4 +1,8 @@
 # Print the multiplication table of a number
 
-Table_need = int(print("Enter which tabel you want to print :- "))
+Table_need = int(input("Enter which tabel you want to print :- "))
+up_tonumber = int(input("Enter till what multipla need :-"))
 
+
+for table in range(1,up_tonumber+1):
+    print(Table_need,"x", table ,"x",table*Table_need)
