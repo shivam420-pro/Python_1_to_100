467b71a7c65e48285787d65079ab91da8f41823a
faclonshivam <faclonshivam1213@gmail.com>
2025-12-27 15:17:29 +0530

    reverse

---
+++ 7 Qestion testing/Reverse_single _digit.py
@@ -0,0 +1,16 @@
+#Input a number and print its reverse.
+#Example:  123 → 321
+
+num = int(input("Enter the number to reverse the digit :- "))
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10   #take the last digit from the digit 
+    
+    reverse = reverse * 10 + digit
+    print(reverse)
+    num = num // 10
+    print(num)
+
+print("Reversed number is:", reverse)
